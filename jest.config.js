ab4d93d1b020780f77301af851bec6ee28d8c049
José Lucas Torquato <jose.torquato@seedz.ag>
2023-11-10 10:57:39 -0300

    config: add configs stated

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  collectCoverageFrom: ['**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/configs',
+    '<rootDir>/src/main.ts',
+    '<rootDir>/modules.d.ts',
+    '<rootDir>/lib',
+  ],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@configs/(.*)$': '<rootDir>/src/configs/$1',
+    '^@error/(.*)$': '<rootDir>/src/error/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@utils/(.*)$': '<rootDir>/utils/$1',
+  },
+  rootDir: '.',
+  setupFilesAfterEnv: ['./test.setup.ts'],
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
