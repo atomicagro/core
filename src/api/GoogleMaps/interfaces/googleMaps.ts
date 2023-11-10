ae9bf146a3ca7b63e89addcfb552e1bfb58a6c10
José Lucas Torquato <jose.torquato@seedz.ag>
2023-11-10 12:13:42 -0300

    fix: imports

--- src/api/GoogleMaps/interfaces/googleMaps.ts
+++ src/api/GoogleMaps/interfaces/googleMaps.ts
@@ -1,5 +1,5 @@
-import { GoogleMapsDto } from '@api/dtos/GoogleMapsDTO';
-import { Coordinates } from '@api/interfaces/coordinates';
+import { GoogleMapsDto } from '@api/GoogleMaps/dtos/GoogleMapsDTO';
+import { Coordinates } from '@api/GoogleMaps/interfaces/coordinates';
 
 export interface GoogleMaps {
   findZipCodeByCoordinates(coordenates: Coordinates, token: string): Promise<GoogleMapsDto>;
