3581fda1f43836f9bcbda39dc121218c0837390d
José Lucas Torquato <jltorquato12@gmail.com>
2023-12-07 16:38:49 -0300

    fix: modified inteface return and add findCityByCoordinates

--- src/api/GoogleMaps/interfaces/googleMaps.ts
+++ src/api/GoogleMaps/interfaces/googleMaps.ts
@@ -1,7 +1,9 @@
-import { GoogleMapsDto } from '@api/GoogleMaps/dtos/GoogleMapsDTO';
+import { CityDto } from '@api/GoogleMaps/dtos/CityDTO';
+import { ZipCodeDto } from '@api/GoogleMaps/dtos/ZipCodeDTO';
 import { Coordinates } from '@api/GoogleMaps/interfaces/coordinates';
 
 export interface GoogleMaps {
-  findZipCodeByCoordinates(coordenates: Coordinates): Promise<GoogleMapsDto>;
+  findCityByCoordinates(coordenates: Coordinates): Promise<CityDto>;
+  findZipCodeByCoordinates(coordenates: Coordinates): Promise<ZipCodeDto>;
   setToken(token: string): void;
 }
